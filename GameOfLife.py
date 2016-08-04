11933ac6e965214eece1cf6ae5239efa7ef403fe
stenman <roger.stenman@gmail.com>
2016-08-04 21:46:58 +0200

    Skeleton of game loop implemented

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,1 +1,43 @@
-print("Hello Game of Life!")
+import pygame
+import time
+
+pygame.init()
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 200,   0,   0)
+GREY     = ( 170, 170, 170)
+
+size = (800, 640)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Game of Life v0.1")
+
+done = False
+
+# Timing
+clock = pygame.time.Clock()
+fps = 60
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("hej doe")
+            done = True
+
+        # Game logic goes here
+
+        # First, clear the screen (no drawing commands above this)
+        screen.fill(GREY)
+
+        # Drawing goes here
+        pygame.draw.rect(screen, RED, [50, 50, 50, 50], 3)
+
+        # Update
+        pygame.display.flip()
+
+        # fps
+        clock.tick(fps)
+
+pygame.quit()
